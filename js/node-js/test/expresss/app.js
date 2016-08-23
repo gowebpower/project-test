de4978fff278eef69abfa0fbea68ccd4d937abe8
Ban Park <yooin15@gmail.com>
2016-08-23 16:39:35 -0700

    setup pug

--- js/node-js/test/expresss/app.js
+++ js/node-js/test/expresss/app.js
@@ -1,17 +1,87 @@
+// ************ 
+  // Import
+
+// ************ 
+
 var express = require('express');
 var app = express();
-
-app.use( express.static('public') );
 
 app.listen(3000, function(){
   console.log('Connected 3000 Port!');
 
 });
 
+
+
+
+// ************ 
+  // Setting
+
+// ************ 
+
+// ------------ Static file
+app.use( express.static('public') );
+
+
+// ------------ Template Engine 
+
+// Set which template engine 
+app.set('view engine', 'pug');
+
+// Set which folder
+app.set('views', './views');
+
+
+
+// ************ 
+  // Pages
+
+// ************ 
+
 app.get('/', function( req, res){
 
-  res.send('Hello Home Page');
+  // res.send('Hello Home Page');
 
-  console.log('hjehe')
+  res.render('index');
+
+  // console.log('hjehe')
+
+});
+
+
+app.get('/about', function( req, res){
+
+  res.send('This is about page');
+
+});
+
+app.get('/contact', function( req, res){
+
+  var list = '';
+
+  for( var i=0; i < 100; i++){
+    list = list + '<li> coding </li>';
+
+  }
+
+
+  var output = `
+    <html>
+      <head>
+        <title></title>
+      </head>
+      <body>
+
+        <h1>asdasd</h1>
+        <div>This is thid</div>
+
+        <ul>
+          ${ list }
+        </ul>
+
+      </body>
+    </html>
+  `
+  res.send(output);
 
 });
