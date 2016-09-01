4410ef44e441f9c7c3a96f94710922f72269460b
bpark <bpark@nexon.net>
2016-09-01 16:29:43 -0700

    node finished basic mysql finished

--- js/node-js/test/expresss/data/db-mysql.js
+++ js/node-js/test/expresss/data/db-mysql.js
@@ -12,16 +12,81 @@ conn.connect();
 
 
 // Basic Call
-var sql = 'select * from topic';
-conn.query(sql, function(err, rows, fields){
-  if(err){
-    console.log(err);
-  } else {
-    console.log('rows', rows);
-    // console.log('fields', fields);
-  }
+// var sql = 'select * from topic';
+// conn.query(sql, function(err, rows, fields){
+//   if(err){
+//     console.log(err);
+//   } else {
 
+//     console.log(rows);
+
+//     // or for cleaner look.
+//     // for(var i=0; i<rows.length; i++){
+//     //   console.log(rows[i].title);
+
+//     // }
+
+//     // fields not important.
+//   }
+
+// });
+
+
+
+// Insert
+
+// var sql = "insert into topic (title, description, author) values(?,?,?)";
+
+// var params = ['nodeJS4','blah blah','Ban Park'];
+
+// conn.query(sql, params, function(err, row, fields){
+//   if(err) console.log(err);
+
+//   else {
+//       console.log(row.insertId);   // show inserted info
+//   }
+// });
+
+
+
+// Update
+
+// notice question mark equals with plain string but they work as variable. 
+// var sql = "update topic set title=?, description=? where id=?";
+
+// var params = ['nodeJSchanged','blah blah', 4];
+
+// conn.query(sql, params, function(err, row, fields){
+//   if(err) console.log(err);
+
+//   else {
+//       console.log(row);   // show inserted info
+//   }
+// });
+
+// Delete
+
+var sql = "delete from topic where id=?";
+
+var params = [3];
+
+conn.query(sql, params, function(err, row, fields){
+  if(err) console.log(err);
+
+  else {
+      console.log(row);   // show inserted info
+  }
 });
+
+
+
+
+
+
+
+
+
+
 
 // Ends connection after call
 conn.end();
