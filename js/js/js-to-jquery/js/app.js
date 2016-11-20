773bdc8b0fce86bcb6adc40f6d79b80dd3580299
Ban Park <yooin15@gmail.com>
2016-11-20 10:54:38 -0800

    js- jquery is added

---
+++ js/js/js-to-jquery/js/app.js
@@ -0,0 +1,231 @@
+
+// var Person = function( name ){
+
+//   this.name = name;
+
+// }
+
+
+
+// var ban = new Person('ban park');
+ 
+
+
+
+//  (function() {
+//   var foo = function(arg) { // core constructor
+//     // ensure to use the `new` operator
+
+ 
+
+//     if (!(this instanceof foo)) {
+//       return new foo(arg);
+//     }
+
+//     // store an argument for this example
+//     this.myArg = arg;
+//     //..
+//   };
+
+//   // create `fn` alias to `prototype` property
+//   foo.fn = foo.prototype = {
+//     //...
+//   };
+
+//   // expose the library
+//   window.foo = foo;
+// })();
+
+// // Extension:
+
+// foo.prototype.myPlugin = function () {
+//   alert(this.myArg);
+//   console.log(this);
+//   return this; // return `this` for chainability
+// };
+
+// foo("bar").myPlugin().myPlugin(); // alerts "bar"
+
+document.addEventListener('DOMContentLoaded', function() {
+  (function(){
+
+    var $$ = function(elems){
+
+      // Need extra code to specify other selector. ( #, css nested selectors and etc )
+      // var allElems = document.body.children;
+      // var i;
+      // var matchedElements = []
+      // for(i in allElems){
+
+      //   console.log( allElems[i] )
+      //   if( allElems[i].classList ){
+      //       if ( allElems[i].classList.contains(elems) ) {
+      //       matchedElements.push( allElems[i] );
+      //     }
+
+      //   }
+        
+      // }
+      
+
+      var matchedElements = document.querySelectorAll(elems);
+      console.log(matchedElements);
+      return new $$.class(matchedElements);
+
+    };
+
+
+    $$.class = function(elems){
+
+      this.elems = elems;
+
+    };
+
+    $$.class.prototype = (function(){
+
+      var addClass = function(className){
+
+        // var i;
+        // for ( i in this.elems ){
+        //   // this.elems[i].classList.add(className);
+        //   console.log(i);
+        //   // console.log ( this.elems[i].classList ) 
+        // }
+
+        // for ( var i = 0, len = this.elems.length; i < len ; i++ ){
+        //   this.elems[i].classList.add(className);
+
+        // }
+
+        this.elems.forEach( function(el){
+          el.classList.add(className);
+        });
+
+        return this;
+      };
+
+      var removeClass = function(className){
+
+        for ( var i = 0, len = this.elems.length; i < len ; i++ ){
+          this.elems[i].classList.remove(className);
+
+        }
+
+        return this;
+      };
+
+      var closest = function(selector){
+        // ref  https://gomakethings.com/climbing-up-and-down-the-dom-tree-with-vanilla-javascript/
+
+        var selectorFirstChar = selector.charAt(0);
+        var findClosestElement;
+        var matchedElements = [];
+        // if selector is a class
+
+        if ( selectorFirstChar === '.' ) { 
+          findClosestElement = findClass;
+        }
+
+        // if selector is an ID
+
+        else if ( selectorFirstChar === '#' ) { 
+          findClosestElement = findClass;
+        }
+
+
+        function findClass(el){
+          if( el.classList.contains( selector.substr(1) ) ){
+            matchedElements.push(el);
+          }
+        }
+
+        function findID(el){
+          if ( el.id === selector.substr(1) ){
+            matchedElements.push(el);
+          }
+        }
+
+        this.elems.forEach( function(el){
+          // if el's tagname is not html and is a element node, keep going and then assign el to el's parentNode
+          for ( ; el.tagName !== 'html' && el.nodeType === 1;  el = el.parentNode ){
+
+            findClosestElement(el);
+
+          }          
+
+        });
+
+        if( matchedElements ){
+          this.elems = matchedElements;
+        }
+
+        return this;
+      };
+
+      var find = function(selector) {
+
+        var matchedElements = [];
+
+        this.elems.forEach( function(el) {
+
+          var elementFound = el.querySelector( selector );
+          if (elementFound) { 
+            matchedElements.push( elementFound );
+          }
+
+        });
+
+        this.elems = matchedElements;
+
+        return this;
+
+      };
+
+      var on = function( action , fn ){
+
+        this.elems.forEach( function(el){
+          el.addEventListener( action, fn );
+        });
+
+        return this;
+      }
+
+      var test = function( ){
+        console.log(this.elems)
+     
+        return this;
+      };
+
+      return {
+
+        addClass: addClass,
+        removeClass: removeClass,
+        closest: closest,
+        find: find,
+        on: on,
+        test: test
+
+      }
+
+    })();
+
+    window.$$ = $$;
+
+  })();
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  $$('.change').addClass('testClass');
+  $$('.change').find('.aaa').addClass('founded');
+  // $$('.aaa').closest('.change').addClass('asdasdasdasdasdasd');
+  // $$('.change').on( 'click', function(e){
+
+  //   console.log(e);
+
+  // });
+ 
+
+});
+
--- js/js/test/js/app.js
+++ js/js/test/js/app.js
@@ -1,47 +1,299 @@
 
-var Person = function( name ){
+// var Person = function( name ){
 
-  this.name = name;
+//   this.name = name;
+
+// }
+
+
+
+// var ban = new Person('ban park');
+ 
+
+
+
+//  (function() {
+//   var foo = function(arg) { // core constructor
+//     // ensure to use the `new` operator
+
+ 
+
+//     if (!(this instanceof foo)) {
+//       return new foo(arg);
+//     }
+
+//     // store an argument for this example
+//     this.myArg = arg;
+//     //..
+//   };
+
+//   // create `fn` alias to `prototype` property
+//   foo.fn = foo.prototype = {
+//     //...
+//   };
+
+//   // expose the library
+//   window.foo = foo;
+// })();
+
+// // Extension:
+
+// foo.prototype.myPlugin = function () {
+//   alert(this.myArg);
+//   console.log(this);
+//   return this; // return `this` for chainability
+// };
+
+// foo("bar").myPlugin().myPlugin(); // alerts "bar"
+
+
+// function getImage( src ){
+//   return new Promise( function(resolve, reject){
+//     var img = new Image();
+//     img.src = src;
+
+//     img.onload = function(){
+//       resolve(src);
+//     }
+
+//     img.onerror = function(){
+//       reject(src);
+//     }
+//   });
+// }
+
+
+// getImage('http://www.w3schools.com/css/img_fjords.jpg').then( function(src){
+
+//   // var imgHtml = "<img src='" + src + "'/>";
+
+//   var imgHtml = document.createElement('img');
+//   imgHtml.setAttribute('src', src);
+
+//   document.getElementById('test').appendChild(imgHtml);
+
+// }, function(src){
+//   alert('error');
+
+// });
+
+
+// getImage('https://cdn.spacetelescope.org/archives/images/large/heic1509a.jpg').then( function(src){
+
+//   // var imgHtml = "<img src='" + src + "'/>";
+
+//   var imgHtml = document.createElement('img');
+//   imgHtml.setAttribute('src', src);
+
+//   document.getElementById('test').appendChild(imgHtml);
+
+// }, function(src){
+//   alert('error');
+
+// });
+
+// getImage('https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688').then( function(src){
+
+//   // var imgHtml = "<img src='" + src + "'/>";
+
+//   var imgHtml = document.createElement('img');
+//   imgHtml.setAttribute('src', src);
+
+//   document.getElementById('test').appendChild(imgHtml);
+
+// }, function(src){
+//   alert('error');
+
+// })
+
+
+
+
+function getImage(src){
+
+  return new Promise(function(resolve, reject){
+
+    var img = new Image();
+    img.src = src;
+
+    img.onload = function(){
+      resolve(src);
+
+    };
+
+    img.onerror = function(){
+      reject(src);
+
+    };
+
+  });
 
 }
 
 
+// getImage('https://cdn.spacetelescope.org/archives/images/large/heic1509a.jpg').then( 
+//   function(src){
 
-var ban = new Person('ban park');
- 
+//     var element = document.createElement('img');
+//     var div = document.createElement('div');
+//     element.src = src;
+//     div.appendChild(element);
+    
+//     console.log(element.width);
+
+//     document.getElementById('test').appendChild(div);
+    
+
+//   }, 
+
+//   function(){
+//     alert('fail');
+//   }
+// )
+
+var images = ['https://cdn.spacetelescope.org/archives/images/large/heic1509a.jpg', 'https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688', 'http://www.w3schools.com/css/img_fjords.jpg'];
+
+
+function displayImages( images ){
+  var targetImage = images.shift();
+
+  if( targetImage ){
+
+    getImage(targetImage).then( 
+      function(src){
+
+        var element = document.createElement('img');
+        var div = document.createElement('div');
+        element.src = src;
+        div.appendChild(element);
+        console.log(element.width);
+
+        document.getElementById('test').appendChild(div);
+        
+
+      }, 
+
+      function(){
+        alert('fail');
+      }
+    )
+
+    displayImages(images);
+  }
+
+  
+
+}
+
+
+// displayImages( images );
 
 
 
- (function() {
-  var foo = function(arg) { // core constructor
-    // ensure to use the `new` operator
 
- 
+function addEvent(elem, evnt, func ){
 
-    if (!(this instanceof foo)) {
-      return new foo(arg);
+  if(elem.attachElement){
+    elem.attachElement( evnt , func );
+  }
+
+
+
+  else {
+
+    if ( typeof elem === 'object' ) {
+
+      console.log( 'obj', elem[0] );
+      elem[0].addEventListener( event, func );
     }
 
-    // store an argument for this example
-    this.myArg = arg;
-    //..
-  };
 
-  // create `fn` alias to `prototype` property
-  foo.fn = foo.prototype = {
-    //...
-  };
-
-  // expose the library
-  window.foo = foo;
-})();
-
-// Extension:
-
-foo.prototype.myPlugin = function () {
-  alert(this.myArg);
-  console.log(this);
-  return this; // return `this` for chainability
+    elem.addEventListener( evnt, func );
+  }
 };
 
-foo("bar").myPlugin().myPlugin(); // alerts "bar"
+// addEvent( document.getElementById('test'), 'click', function(){ alert('asdasd')} );
+
+setTimeout( function(){
+  var images = document.getElementsByTagName('img');
+
+
+
+  addEvent( images, 'click', function(e){ alert(e)} );
+
+
+
+  } , 5000
+
+
+)
+
+
+// document.getElementById('test').addEventListener('click', function(){ alert('asd')});
+
+
+function dReplace( someClass, content){
+
+  var elems = document.getElementsByTagName('*'), i;
+
+  for (i in elems){
+
+    if ( elems[i].className === someClass){
+      elems[i].innerHTML = content
+    }
+
+  }
+
+  console.log(elems);
+  // someClass.innerHTML = ( content );
+
+}
+
+dReplace('change', 'This is new');
+
+
+function $( className ){
+
+  var i;
+  var elems = document.body.children
+  var matchedElements = [];
+  for( i in elems){
+    if(elems[i].className === className){
+      matchedElements.push(elems[i]);
+    }
+  }
+
+  return new $Instance( 'matchedElements' );
+}
+
+
+$Instance = function( elems ){
+  this.elems = elems;
+  
+}
+
+$Instance.prototype = function(){
+  var addClass = function(){};
+  var removeClass = function(){};
+  var find = function(){};
+  // so on..
+
+
+  return {
+    addClass: addClass,
+    removeClass: removeClass,
+    find: find
+  }
+}
+
+  
+
+
+ 
+
+
+var something = getElementsByClassName('change');
+
+console.log( something[0].offsetWidth);
+
+
